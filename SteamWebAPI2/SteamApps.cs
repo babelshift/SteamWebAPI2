6759bd58275015cbd04aeb1554f2d8e6c57c9299
babelshift <justin.d.skiles@gmail.com>
2015-11-23 12:20:47 -0500

    Added wrappers for SteamApps / GetAppList method.

---
+++ SteamWebAPI2/SteamApps.cs
@@ -0,0 +1,24 @@
+﻿using SteamWebAPI2.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamWebAPI2
+{
+    public class SteamApps : SteamWebInterface
+    {
+        public SteamApps(string steamWebApiKey)
+            : base(steamWebApiKey, "ISteamApps")
+        {
+        }
+
+        public async Task<IReadOnlyCollection<SteamApp>> GetAppList()
+        {
+            var steamAppList = await CallMethodAsync<SteamAppListResultContainer>("GetAppList", 1);
+            return new ReadOnlyCollection<SteamApp>(steamAppList.AppListResult.AppList.Apps);
+        }
+    }
+}
