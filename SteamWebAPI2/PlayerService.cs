41f67f1eb4919db3fc316b39fe87f55ebec0fcca
babelshift <justin.d.skiles@gmail.com>
2015-11-24 08:54:22 -0500

    Added wrappers for GetRecentlyPlayedGames method.

---
+++ SteamWebAPI2/PlayerService.cs
@@ -0,0 +1,25 @@
+﻿using SteamWebAPI2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamWebAPI2
+{
+    public class PlayerService : SteamWebInterface
+    {
+        public PlayerService(string steamWebApiKey)
+            : base(steamWebApiKey, "IPlayerService")
+        {
+        }
+
+        public async Task<RecentlyPlayedGameResult> GetRecentlyPlayedGamesAsync(long steamId)
+        {
+            List<SteamWebRequestParameter> parameters = new List<SteamWebRequestParameter>();
+            AddToParametersIfHasValue("steamid", steamId, parameters);
+            var recentlyPlayedGameResult = await CallMethodAsync<RecentlyPlayedGameResultContainer>("GetRecentlyPlayedGames", 1, parameters);
+            return recentlyPlayedGameResult.Result;
+        }
+    }
+}
