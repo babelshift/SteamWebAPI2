cd07c22950f09f21d94ed0cd913a05fcff262c07
babelshift <justin.d.skiles@gmail.com>
2015-11-24 09:07:39 -0500

    Added wrappers for GetOwnedGames method.

--- SteamWebAPI2/PlayerService.cs
+++ SteamWebAPI2/PlayerService.cs
@@ -14,6 +14,29 @@ namespace SteamWebAPI2
         {
         }
 
+        public async Task<OwnedGamesResult> GetOwnedGamesAsync(long steamId, bool? includeAppInfo = null, bool? includeFreeGames = null, IReadOnlyCollection<int> appIdsToFilter = null)
+        {
+            int? includeAppInfoBit = 0;
+            if (includeAppInfo.HasValue) { includeAppInfoBit = includeAppInfo.Value ? 1 : 0; }
+
+            int? includeFreeGamesBit = 0;
+            if (includeFreeGames.HasValue) { includeFreeGamesBit = includeFreeGames.Value ? 1 : 0; }
+
+            List<SteamWebRequestParameter> parameters = new List<SteamWebRequestParameter>();
+            AddToParametersIfHasValue("steamid", steamId, parameters);
+            AddToParametersIfHasValue("include_appinfo", includeAppInfoBit, parameters);
+            AddToParametersIfHasValue("include_played_Free_games", includeFreeGamesBit, parameters);
+
+            if (appIdsToFilter != null)
+            {
+                string appIdsDelimited = String.Join(",", appIdsToFilter);
+                AddToParametersIfHasValue("appids_filter", appIdsDelimited, parameters);
+            }
+
+            var ownedGamesResult = await CallMethodAsync<OwnedGamesResultContainer>("GetOwnedGames", 1, parameters);
+            return ownedGamesResult.Result;
+        }
+
         public async Task<RecentlyPlayedGameResult> GetRecentlyPlayedGamesAsync(long steamId)
         {
             List<SteamWebRequestParameter> parameters = new List<SteamWebRequestParameter>();
