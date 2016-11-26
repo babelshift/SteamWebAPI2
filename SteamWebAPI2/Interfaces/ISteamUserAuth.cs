279fdb9ec87e71f8d72224abc5cb46bad4a0cd95
Justin <justin.d.skiles@gmail.com>
2016-11-26 16:25:52 -0500

    Added method to AuthenticateUserTicket for Issue #25.

---
+++ SteamWebAPI2/Interfaces/ISteamUserAuth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamWebAPI2.Interfaces
+{
+    public interface ISteamUserAuth
+    {
+        /// <summary>
+        /// Authenticates a Steam User based on a User Ticket from GetAuthSessionTicket
+        /// </summary>
+        /// <param name="appId">App ID of the game to authenticate against</param>
+        /// <param name="ticket">Ticket from GetAuthSessionTicket</param>
+        /// <returns>Results of authentication request</returns>
+        Task<dynamic> AuthenticateUserTicket(int appId, string ticket);
+    }
+}
---
+++ SteamWebAPI2/Interfaces/SteamUserAuth.cs
@@ -0,0 +1,29 @@
+﻿using SteamWebAPI2.Utilities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SteamWebAPI2.Interfaces
+{
+    public class SteamUserAuth : SteamWebInterface, ISteamUserAuth
+    {
+        public SteamUserAuth(string steamWebApiKey)
+            : base(steamWebApiKey, "ISteamUserAuth")
+        {
+        }
+
+        /// <summary>
+        /// Authenticates a Steam User based on a User Ticket from GetAuthSessionTicket
+        /// </summary>
+        /// <param name="appId">App ID of the game to authenticate against</param>
+        /// <param name="ticket">Ticket from GetAuthSessionTicket</param>
+        /// <returns>Results of authentication request</returns>
+        public async Task<dynamic> AuthenticateUserTicket(int appId, string ticket)
+        {
+            List<SteamWebRequestParameter> parameters = new List<SteamWebRequestParameter>();
+            parameters.AddIfHasValue(appId, "appid");
+            parameters.AddIfHasValue(ticket, "ticket");
+            var playingSharedGameResult = await CallMethodAsync<dynamic>("AuthenticateUserTicket", 1, parameters);
+            return playingSharedGameResult;
+        }
+    }
+}
