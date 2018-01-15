945550d3bad7b4e7f5b33975e7d2d0409bfef880
Justin <justin.d.skiles@gmail.com>
2018-01-15 07:09:18 -0500

    Fix SemVer.

--- default.ps1
+++ default.ps1
@@ -37,7 +37,6 @@ task compile -depends clean {
   $projectPath = "$sourceDir\SteamWebAPI2\SteamWebAPI2.csproj"
 
   $version = if ($env:APPVEYOR_BUILD_NUMBER -ne $NULL) { $env:APPVEYOR_BUILD_NUMBER } else { '0' }
-  $version = "{0:D5}" -f [convert]::ToInt32($version, 10)
 
   exec { dotnet restore $projectPath }
   exec { dotnet build $projectPath -c $config }
