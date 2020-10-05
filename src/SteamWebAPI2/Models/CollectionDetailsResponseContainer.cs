37fc0898290f3b68f67238c26e4b8ca7045a65d7
Oliver <8377772+oliver4888@users.noreply.github.com>
2020-10-05 22:01:53 +0100

    Move CollectionDetailsResponseContainer and add JsonProperty attributes.

--- src/SteamWebAPI2/Models/CollectionDetailsResponseContainer.cs
+++ src/SteamWebAPI2/Models/CollectionDetailsResponseContainer.cs
@@ -1,30 +1,47 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
-namespace Steam.Models
+namespace SteamWebAPI2.Models
 {
     public class CollectionDetailsResponseContainer
     {
+        [JsonProperty("response")]
         public CollectionDetailsResponse Response { get; set; }
     }
 
     public class CollectionDetailsResponse
     {
+        [JsonProperty("result")]
         public uint Result { get; set; }
+
+        [JsonProperty("resultcount")]
         public uint ResultCount { get; set; }
+
+        [JsonProperty("collectiondetails")]
         public IList<CollectionDetail> CollectionDetails { get; set; }
     }
 
     public class CollectionDetail
     {
+        [JsonProperty("publishedfileid")]
         public string PublishedFileId { get; set; }
+
+        [JsonProperty("result")]
         public uint Result { get; set; }
+
+        [JsonProperty("children")]
         public IList<CollectionDetailItem> Children { get; set; }
     }
 
     public class CollectionDetailItem
     {
+        [JsonProperty("publishedfileid")]
         public string PublishedFileId { get; set; }
+
+        [JsonProperty("sortorder")]
         public uint SortOrder { get; set; }
+
+        [JsonProperty("filetype")]
         public uint FileType { get; set; }
     }
 }
