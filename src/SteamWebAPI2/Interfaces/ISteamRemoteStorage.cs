ea9d647ccae51cace76f2794e6a3ed7d597c5644
Oliver <8377772+oliver4888@users.noreply.github.com>
2020-10-05 22:35:05 +0100

    Add collection methods to ISteamRemoteStorage.

--- src/SteamWebAPI2/Interfaces/ISteamRemoteStorage.cs
+++ src/SteamWebAPI2/Interfaces/ISteamRemoteStorage.cs
@@ -1,13 +1,17 @@
 ﻿using Steam.Models;
+using SteamWebAPI2.Models;
 using SteamWebAPI2.Utilities;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace SteamWebAPI2.Interfaces
 {
-    using System.Collections.Generic;
-
     public interface ISteamRemoteStorage
     {
+        Task<ISteamWebResponse<IReadOnlyCollection<CollectionDetail>>> GetCollectionDetails(ulong collectionId);
+
+        Task<ISteamWebResponse<IReadOnlyCollection<CollectionDetail>>> GetCollectionDetails(IList<ulong> collectionId);
+
         Task<ISteamWebResponse<IReadOnlyCollection<PublishedFileDetailsModel>>> GetPublishedFileDetailsAsync(uint itemCount, IList<ulong> publishedFileIds);
 
         Task<ISteamWebResponse<IReadOnlyCollection<PublishedFileDetailsModel>>> GetPublishedFileDetailsAsync(IList<ulong> publishedFileIds);
