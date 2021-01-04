663433d41ad1932f263438d71b62b6944cff2532
Luke Parker <lukemparker@outlook.com>
2021-01-04 22:39:17 +1000

    Convert AuthenticateUserTicket to strong typing

---
+++ src/Steam.Models/SteamUserAuth/SteamUserAuthResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Steam.Models.SteamUserAuth
+{
+    public class SteamUserAuthResponse
+    {
+        public SteamAuthResponseParams Params { get; set; }
+        public SteamAuthError Error { get; set; }
+        public bool Success => Error == null && Params != null;
+    }
+}
--- src/SteamWebAPI2/Interfaces/SteamUserAuth.cs
+++ src/SteamWebAPI2/Interfaces/SteamUserAuth.cs
@@ -3,6 +3,7 @@ using SteamWebAPI2.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Steam.Models.SteamUserAuth;
 
 namespace SteamWebAPI2.Interfaces
 {
@@ -30,12 +31,12 @@ namespace SteamWebAPI2.Interfaces
         /// <param name="appId">App ID of the game to authenticate against</param>
         /// <param name="ticket">Ticket from GetAuthSessionTicket</param>
         /// <returns>Results of authentication request</returns>
-        public async Task<ISteamWebResponse<dynamic>> AuthenticateUserTicket(uint appId, string ticket)
+        public async Task<ISteamWebResponse<SteamUserAuthResponseModel>> AuthenticateUserTicket(uint appId, string ticket)
         {
             List<SteamWebRequestParameter> parameters = new List<SteamWebRequestParameter>();
             parameters.AddIfHasValue(appId, "appid");
             parameters.AddIfHasValue(ticket, "ticket");
-            var playingSharedGameResult = await steamWebInterface.GetAsync<dynamic>("AuthenticateUserTicket", 1, parameters);
+            var playingSharedGameResult = await steamWebInterface.GetAsync<SteamUserAuthResponseModel>("AuthenticateUserTicket", 1, parameters);
             return playingSharedGameResult;
         }
     }
