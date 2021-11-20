7ccee33dd6079397b2d48334a263568f2946dccb
Andrey Kornev <read_maniak@mail.ru>
2021-11-20 17:10:40 +0300

    support for custom SteamWebApiBaseUrl (#122)

--- src/SteamWebAPI2/Utilities/SteamWebInterfaceFactoryOptions.cs
+++ src/SteamWebAPI2/Utilities/SteamWebInterfaceFactoryOptions.cs
@@ -3,5 +3,6 @@ namespace SteamWebAPI2.Utilities
     public class SteamWebInterfaceFactoryOptions
     {
         public string SteamWebApiKey { get; set; }
+        public string SteamWebApiBaseUrl { get; set; }
     }
 }
